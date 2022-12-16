486532ced1418ef5f56988d0a7871292f1c384d8
jccaldw1 <jacobccaldwell@outlook.com>
2022-12-15 23:27:13 -0700

    removed a comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,6 @@
 
 <h1>Welcome to Jacob's Website</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<!-- <a href="/Resume">Resume</a>
-<a href="/About">About</a> -->
 {#each routes as route}
     <a href="/{route}">{route}</a>
 {/each}
