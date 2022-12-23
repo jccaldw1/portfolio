0188432132870f95e24fda42598403c199eca119
jccaldw1 <jacobccaldwell@outlook.com>
2022-12-22 21:34:28 -0700

    Add test to test db connection parameter

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,9 @@
 import { routes } from '$lib/route-configuration';
 import { expect, test } from '@playwright/test';
+import { MONGO_DB_URI } from '$lib/secrets';
+import { MongoClient } from 'mongodb';
+import User from '$lib/server/authentication-model/User';
+import { sortUserPlugins } from 'vite';
 
 test('make sure all pages in route configuration are accessible', async ({page}) => {
 	// This cannot be done in <array>.forEach(async () => {}), possibly because of async function wrapped in an async function.
@@ -13,3 +17,23 @@ test('make sure all pages in route configuration are accessible', async ({page})
 });
 
 //TODO: test that adds and removes a user to/from the database to verify db connection works.
+test('verify db connection is good', async () => {
+	const client = new MongoClient(MONGO_DB_URI);
+
+	await client.connect();
+	let database = client.db('Users');
+	let users = database.collection<User>('Users');
+
+	let testAddUser = await users.insertOne(new User({
+		username: "testUser",
+		hashedpassword: "testPass"
+	}));
+
+	expect(testAddUser).toBeTruthy();
+
+	const deleteQuery = { username: "testUser" };
+
+	let testRemoveUser = await users.deleteOne(deleteQuery);
+
+	expect(testRemoveUser.acknowledged).toBeTruthy();
+})
