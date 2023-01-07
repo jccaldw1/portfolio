d022fea3e743809a18df7ba9721b1f86d055847a
jccaldw1 <jacobccaldwell@outlook.com>
2023-01-07 01:19:04 -0700

    added tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
-    import { page } from '$app/stores'
+    import { page } from "$app/stores";
     import { routes } from "$lib/route-configuration";
+
+    import "../app.css";
 </script>
 
 <div class="app">
@@ -13,8 +15,10 @@
         {/each}
     </div>
 
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
     <main>
-        <slot/>
+        <slot />
     </main>
 
     <footer>
