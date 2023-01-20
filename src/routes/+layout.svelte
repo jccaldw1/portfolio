e72e495dbe9e1a6318c87e5b52132a85a3a98be4
jccaldw1 <jacobccaldwell@outlook.com>
2023-01-19 19:03:44 -0700

    inline navbar buttons and progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,29 +8,37 @@
     <link rel="stylesheet" href="css/pico.min.css" />
 </head>
 
-<div class="app container">
-    <div class="grid">
-        <div align="left">
-            {#if $page.route.id !== "/"}
-                <a href="/">Go home.</a>
-            {/if}
-        </div>
-        <div align="right">
-            <div class="grid">
-                {#each routes as route}
-                    <!-- <button><a href="/{route}">{route}</a></button>&emsp; -->
-                    <!-- <button>{route}</button>&emsp; -->
-                    <a href="/{route}">{route}</a>&emsp;
-                {/each}
+<div class="app">
+    <div class="container-fluid">
+        <div class="grid">
+            <!-- logo -->
+            <div />
+            <div align="left">
+                {#if $page.route.id !== "/"}
+                    <a role="button" class="outline" href="/">Go home.</a>
+                {/if}
+            </div>
+            <div align="right">
+                <div class="grid">
+                    {#each routes as route}
+                        <!-- <button><a href="/{route}">{route}</a></button>&emsp; -->
+                        <!-- <button>{route}</button>&emsp; -->
+                        <a role="button" class="outline" href="/{route}"
+                            >{route}</a
+                        >&emsp;
+                    {/each}
+                </div>
             </div>
         </div>
     </div>
 
-    <main>
-        <slot />
-    </main>
+    <div class="container">
+        <main>
+            <slot />
+        </main>
 
-    <footer>
-        <p>Check out my <a href="https://github.com/jccaldw1">GitHub</a></p>
-    </footer>
+        <footer>
+            <p>Check out my <a href="https://github.com/jccaldw1">GitHub</a></p>
+        </footer>
+    </div>
 </div>
