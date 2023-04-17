6bd66419d9758d13db502363fbf29e7a30ccf5d8
jccaldw1 <jacobccaldwell@outlook.com>
2023-04-16 22:50:05 -0700

    add tailwind styling to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,52 @@
 <script>
     import { page } from "$app/stores";
     import { routes } from "$lib/route-configuration";
+    
+    let buttonClasses = "rounded-full bg-slate-700 p-2 mx-2 border";
+
+    /**
+     * A div with this class will push other divs to the center of the screen by this margin.
+     * Divs with this class should have ONLY this class.
+     * @type {string}
+    */
+    let spacingClass = "basis-1/5";
 </script>
 
 <div class="app">
-    <div class="container-fluid">
-        <div class="grid">
-            <!-- logo -->
+    <!-- navbar -->
+        <!-- logo (tbd)-->
 
-            <div>
-                {#if $page.route.id !== "/"}
-                    <a role="button" class="outline" href="/">Go home.</a>
-                {:else}
-                    <div/>
-                {/if}
-                {#each routes as route}
-                    <!-- <button><a href="/{route}">{route}</a></button>&emsp; -->
-                    <!-- <button>{route}</button>&emsp; -->
-                    <a role="button" class="outline" href="/{route}">{route}</a
-                    >&emsp;
-                {/each}
-            </div>
-        </div>
+    <div class="flex flex-row justify-end bg-slate-900 sticky top-0 py-3">
+        <div class={spacingClass}/>
+
+        {#if $page.route.id !== "/"}
+            <a role="button" class={buttonClasses} href="/">Go home.</a>
+        {/if}
+
+        {#each routes as route}
+            <a role="button" class={buttonClasses} href="/{route}">{route}</a>&emsp;
+        {/each}
+
+        <div class={spacingClass}/>
     </div>
 
-    <div class="container">
+    <div class="flex flex-row">
+        <div class={spacingClass}/>
+
         <main>
             <slot />
         </main>
 
+        <div class={spacingClass}/>
+    </div>
+
+    <div class="flex flex-row">
+        <div class={spacingClass}/>
+
         <footer>
             <p>Check out my <a href="https://github.com/jccaldw1">GitHub</a></p>
         </footer>
+
+        <div class={spacingClass}/>
     </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
-
-<h1>Welcome!</h1>
 <h6>Welcome to Jacob's website! Take a look at my progress:</h6>
-<progress max="100" value="20"></progress>
+<progress max="100" value="20"/>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
