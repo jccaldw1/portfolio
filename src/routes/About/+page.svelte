bb487725ffd37292d62b8d1456e41d6df86f93bd
jccaldw1 <jacobccaldwell@outlook.com>
2023-04-16 22:50:22 -0700

    Add an image to my about page

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     import Contact from "./Contact.svelte";
+    import Image from "./Image.svelte";
 </script>
+
+<head>
+    <title>About Jacob!!!!!</title>
+</head>
+
 <p>This is my about section</p>
-<Contact></Contact>
+
+<Image/>
+<Contact/>
---
+++ src/routes/About/Image.svelte
@@ -0,0 +1,3 @@
+
+
+<img class="rounded-3xl" style="width: {500}px;" src="IMG_1215.jpg" alt="Jacob Caldwell at a wedding"/>
